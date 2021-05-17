a6f1e2a3092a66a24b02609bff924a6a4d01e460
ysh4296 <dbtmdgns4296@naver.com>
2021-05-18 01:23:24 +0900

    Create 9084.cpp

---
+++ BOJ/9084.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int test_case;
+    cin >> test_case;
+    int coin[21] = {0,};
+    int N;
+    int M;
+    for(int i = 0 ; i < test_case ; i++){
+        cin >> N;
+        memset(coin,0,sizeof(coin));
+        for(int a = 1 ; a <= N ; a++){
+            cin >> coin[a];
+        }
+        cin >> M;
+        int dp[10001] = {0,};
+        memset(dp,0,sizeof(dp));
+        dp[0] = 1;
+        for(int a = 1 ; a <= N ; a++){
+            for(int b = coin[a] ; b <= M ; b++){
+                dp[b] += dp[b - coin[a]];
+            }
+        }
+        cout << dp[M] << endl;
+    }
+    return 0;
+}
