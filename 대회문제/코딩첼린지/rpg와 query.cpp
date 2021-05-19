2de3f87357fb03404d377c032716128ed63b9d52
ysh4296 <dbtmdgns4296@naver.com>
2021-05-20 00:32:42 +0900

    Update rpg와 query.cpp

--- 대회문제/코딩첼린지/rpg와 query.cpp
+++ 대회문제/코딩첼린지/rpg와 query.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <algorithm>
 #include <iostream>
+#define INF 1e9
 using namespace std;
 typedef long long LL;
 struct point{
@@ -18,14 +19,20 @@ struct point{
 };
 vector<long long> solution(int n, int z, vector<vector<int>> roads, vector<long long> queries) {
     vector<long long> answer;
-    LL MAX = z*z+1;
+    LL MAX = z*z;
     LL dp[n][MAX];
     LL ans[MAX];
-    fill(dp[0] , dp[0] + n*MAX , 2555);
-    fill(ans , ans + MAX , 2555);
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < MAX ; j++){
+            dp[i][j] = INF;
+        } 
+    }
+    for(int i = 0 ; i < MAX ; i++){
+        ans[i] = INF;
+    }
     vector<point> Edge[n]; //next city, spend coin,0
-    for(int i = 0 ; i < (int)roads.size() ; i++){
-        Edge[roads[i][0]].push_back(point(roads[i][1],roads[i][2],0));
+    for(vector<int> road : roads){
+        Edge[road[0]].push_back(point((LL)road[1],(LL)road[2],0));
     }
     queue<point> q;
     point start = point(0,0,0);
@@ -36,30 +43,35 @@ vector<long long> solution(int n, int z, vector<vector<int>> roads, vector<long 
         if(ans[cur.coin] > cur.day) {
             ans[cur.coin] = cur.day;
             for(int next_city = 0 ; next_city < n ; next_city++){ // type 3
-                if(next_city == cur.city) continue;
                 q.push(point(next_city, cur.coin, cur.day+1));
             }
         }
-        if(dp[cur.city][cur.coin] <= cur.day) continue;
+        if(dp[cur.city][cur.coin] < cur.day) continue;
         dp[cur.city][cur.coin] = cur.day;
         for(point next_city : Edge[cur.city]){ //type 1
             q.push(point(next_city.city, cur.coin + next_city.coin, cur.day+1));
         }
     }
-    for(int i = z ; i <= MAX ; i++){ // type 2
-        if(ans[i-z] + 1 > 2555) continue;
+    for(int i = z ; i < MAX ; i++){ // type 2
+        if(ans[i-z] == INF) continue;
         ans[i] = min(ans[i],ans[i-z] + 1);
     }
     for(LL tcoin : queries){
         LL div = 0;
-        if(ans[tcoin % (MAX-1)] < 2555){
-            if(tcoin >= MAX-1){
-                div = ((tcoin) - (tcoin%(MAX-1)))/z;
+        LL temp = tcoin%(z);
+        LL sol = 100000000000000001;
+        for(LL i = 0 ; i < z ; i++){
+            if(ans[temp+i*z] != INF){
+                div = (LL)(tcoin - tcoin%z)/z;
+                if(sol > ans[temp + i*z]+div-i)
+                sol = ans[temp + i*z]+div-i;
             }
-            answer.push_back(ans[tcoin%(MAX-1)] + div);
         }
-        else
+        if(sol == 100000000000000001){
             answer.push_back(-1);
+        } else {
+            answer.push_back(sol);
+        }
     }
     return answer;
 }
