5157e856f35eab7c26897cb3e917450d15143a25
ysh4296 <dbtmdgns4296@naver.com>
2021-07-29 20:27:14 +0900

    Create Topology_sort.cpp

---
+++ basic_code/Topology_sort.cpp
@@ -0,0 +1,27 @@
+int indegree[MAX];
+vector<int> adj[MAX];
+int result[MAX];
+queue<int> q;
+for (int i = 1; i <= N; i++) {
+  if (indegree[i] == 0) q.push(i);
+}
+int cnt = 0;
+while (!q.empty() && cnt < N) {
+  int cur = q.front();
+  q.pop();
+  cnt++;
+  result[cnt] = cur;
+  for (int next : adj[cur]) {
+    if (--indegree[next] == 0) {
+      q.push(next);
+    }
+  }
+}
+if (cnt < N) { // can't reach every node == found cycle
+  cout << 0 << "\n";
+}
+else {//print sorted order
+  for (int i = 1; i <= N; i++) {
+    cout << result[i] << "\n";
+  }
+}
