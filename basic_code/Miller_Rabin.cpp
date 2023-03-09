23e6a42ce60fd2646f5a37a2a3a02b5422507afa
ysh4296 <dbtmdgns4296@naver.com>
2023-03-09 21:56:53 +0900

    Update Miller_Rabin.cpp

--- basic_code/Miller_Rabin.cpp
+++ basic_code/Miller_Rabin.cpp
@@ -1,8 +1,19 @@
-bool miller_rabin(ll N,ll a) { // is N prime? calculate with nature number a
+int modular(int a, int k , int N){
+    int res = 1;
+    a %= N;
+    while(k) {
+        if(k%2) res = (res*a) % N;
+        k /= 2;
+        a = (a*a) % N;
+    }
+    return res;
+}
+
+bool miller_rabin(int N,int a) { // is N prime? calculate with nature number a
     if(a % N == 0) return true;
-    ll k = N-1; // k means N-1
+    int k = N-1; // k means N-1
     while(1) {
-        ll temp = modular(a, k, N); // a^k mod N
+        int temp = modular(a, k, N); // a^k mod N
         //a^N-1 (mod N) == 1 is Fermat's little Theorem
         if(temp == N-1) return true; // a^k = -1 (mod N)
         if (k % 2) return (temp == 1 || temp == N-1); a^(2*d + 1) = 1,-1(mod N)
