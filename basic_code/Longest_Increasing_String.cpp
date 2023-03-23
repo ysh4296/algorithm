c301923a824cf02913310c254c28e40016a26307
ysh4296 <dbtmdgns4296@naver.com>
2023-03-23 10:40:45 +0900

    Update Longest_Increasing_String.cpp

--- basic_code/Longest_Increasing_String.cpp
+++ basic_code/Longest_Increasing_String.cpp
@@ -4,13 +4,13 @@
 vector<int> arr;
 vector<int> dp(n, 1);
 
-int LIS() {
+int LIS() {// n^2의 시간복잡도를 가지는 알고리즘 dp[i] 배열에는 i번째 원소까지 배열 중 lis의 길이가 저장된다.
     int n = arr.size();
     int result = 1;
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (arr[i] > arr[j]) {
-                dp[i] = max(dp[i], dp[j] + 1);
+                dp[i] = max(dp[i], dp[j] + 1); // 현재값과 새로 발생하는 length중 큰값을 저장
             }
         }
         result = max(result, dp[i]);
@@ -20,17 +20,19 @@ int LIS() {
 
 vector<int> arr;
 vector<int> dp(n, 0);
-int LIS() {
+
+int LIS() {// nlog(n)의 시간복잡도를 가지는 알고리즘 dp 배열은 각 위치에 대해 해당 인덱스 길이의 LIS의 마지막 원소 중 가능한 가장 작은 값을 유지한다.
     int n = arr.size();
     int length = 0;
 
     for (int i = 0; i < n; i++) {
-        auto it = lower_bound(dp.begin(), dp.begin() + length, arr[i]);
-        if (it == dp.begin() + length) {
-            dp[length++] = arr[i];
+        int index = lower_bound(dp.begin(), dp.begin() + length, arr[i]) - dp.begin(); // lowerbonud함수를 통한 index 찾기
+        if (index == length) { // can add current index
+            dp.push_back(arr[i]);
+            length++;
         }
-        else {
-            *it = arr[i];
+        else { // can optimize
+            dp[index] = arr[i];
         }
     }
 
