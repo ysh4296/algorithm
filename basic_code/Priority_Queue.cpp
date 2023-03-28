a203be893def58ee568db66d349de7569ee8fe92
ysh4296 <dbtmdgns4296@naver.com>
2023-03-28 15:13:28 +0900

    Update Priority_Queue.cpp

--- basic_code/Priority_Queue.cpp
+++ basic_code/Priority_Queue.cpp
@@ -3,4 +3,26 @@ struct cmp{
     return a > b;// make priority_queue in increasing order 
   }
 };
+
 priority_queue<int, vector<int>, cmp> pq;
+
+struct Point {
+    public:
+        int value,row,column;
+        Point(int v, int r, int c) { // constructor
+            value = v;
+            row = r;
+            column = r;
+        } 
+};
+
+struct compare {
+    bool operator()(Point &a, Point &b) {
+        if(a.value != b.value) return a.value > b.value; // smaller value comes top
+        else if(a.row != b.row) return  a.row > b.row; // smaller row comes top
+        else if (a.column != b.column) return a.column > b.column; // smaller column comes top
+        else return true;
+    };
+};
+
+priority_queue <Point,vector<Point>,compare> pq;
